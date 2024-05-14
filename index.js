4c53df14596e6fa6ef9c1ccdf4a969354888ef18
Alexia <96691787+kitsunebunny@users.noreply.github.com>
2024-05-14 11:41:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const RPC = require("discord-rpc");
+const rpc = new RPC.Client({
+    transport: "ipc"
+})
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "Editing Sequence 2",
+        state: "Untitled.vpj",
+        largeImageKey: "image",
+        largeImageText: "Using Videopad Pro v 8.63",
+    })
+
+    console.log("RPC active");
+})
+
+rpc.login({
+    clientId: "1205513720277639219"
